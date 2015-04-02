1af5b61bb456b4db534403f14b2b85823774d944
Torsten Seemann <torsten.seemann@gmail.com>
2015-04-02 15:28:43 +1100

    FigTree 1.4.2 (new formula)

---
+++ figtree.rb
@@ -0,0 +1,21 @@
+class Figtree < Formula
+  homepage "http://tree.bio.ed.ac.uk/software/figtree/"
+  # doi ""
+  # tag "bioinformatics"
+
+  url 'http://tree.bio.ed.ac.uk/download.php?id=90&num=3'
+  sha256 "5064509fbe8cf069c6b0f8fdacf8c3f651971b11956484ce98a18a114ce1a096"
+  version "1.4.2"
+
+  depends_on :java
+
+  def install
+    rm "bin/figtree"
+    bin.write_jar_script lib/"figtree.jar", "figtree"
+    prefix.install Dir["*"]
+  end
+
+  test do
+    # No test because this is a GUI
+  end
+end
