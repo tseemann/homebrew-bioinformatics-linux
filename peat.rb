1896bf4fe0eb2f939c4733d6c9a949a0d0b1575e
Torsten Seemann <torsten.seemann@gmail.com>
2015-06-07 10:34:03 +1000

    PEAT 1.0 (update)

--- peat.rb
+++ peat.rb
@@ -1,25 +1,26 @@
 class Peat < Formula
+  desc "De novo NGS read trimmer which automatically learns adaptor sequences"
   homepage "https://github.com/jhhung/PEAT/"
+  # doi "10.1186/1471-2105-16-S1-S2"
   # tag "bioinformatics"
-  # doi ""
 
-  version "1.0b"
+  version "1.0"
 
   if OS.mac?
     url "https://github.com/jhhung/PEAT/releases/download/v1.0-beta.1/PEAT_mac_v1.0-beta.1"
     sha256 "900ae5e72e7c4fb961fb59d45d82f51350a2d44efa5f932f3c872597438be9b2"
   elsif OS.linux?
-    url "https://github.com/jhhung/PEAT/releases/download/v1.0-beta.1/PEAT_linux_v1.0-beta.1"
-    sha256 "1a780a32d3245efef62d3ee34dc7a7dd84ad09ec3c631da1968bc3cc2d2869e2"
+    url "https://github.com/jhhung/PEAT/releases/download/v1.0/PEAT_v.1.0_linux_static"
+    sha256 "c7c85c85393bc0780699cfc0773cfacd3e8c117347181bdcda4ada2f152bec37"
   else
-    fail "Unsupported operating system"
+    raise "Unsupported operating system"
   end
 
   def install
     if OS.mac?
       bin.install "PEAT_mac.v1.0-beta.1" => "PEAT"
     else
-      bin.install "PEAT_linux_v1.0-beta.1" => "PEAT"
+      bin.install "PEAT_v.1.0_linux_static" => "PEAT"
     end
   end
 
