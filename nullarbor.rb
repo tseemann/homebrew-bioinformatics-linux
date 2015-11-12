36ef96504ff08234c63897d665cc7bbc062a0c43
Torsten Seemann <torsten.seemann@gmail.com>
2015-11-12 15:35:33 +1100

    Nullarbor 0.8 (update)

--- nullarbor.rb
+++ nullarbor.rb
@@ -3,20 +3,25 @@ class Nullarbor < Formula
   homepage "https://github.com/tseemann/nullarbor"
   # doi ""
   # tag "bioinformatics"
-  url "https://github.com/tseemann/nullarbor/archive/v0.7.tar.gz"
-  sha256 "7dbff1ae6255b5e4f1e0a9bbb0104af34ecf2a07d28d771bd584fb7a5409c34b"
+
+  url "https://github.com/tseemann/nullarbor/archive/v0.8.tar.gz"
+  sha256 "c103daff9c1b205da65a10bf34ce68f5e732fa77a8393d86cbeb2d842bd24f53"
+
+  head "https://github.com/tseemann/nullarbor.git"
 
   depends_on "Bio::Perl" => :perl
   depends_on "Time::Piece" => :perl
   depends_on "Moo" => :perl
   depends_on "Data::Dumper" => :perl
   depends_on "Spreadsheet::Read" => :perl
+  depends_on "File::Copy" => :perl
+  depends_on "YAML::Tiny" => :perl
   depends_on "Bio::Roary" => :perl
 
   depends_on "tseemann/bioinformatics-linux/snippy"
   depends_on "prokka"
-  depends_on "trimal"
-  depends_on "fasttree"
+  depends_on "mash"
+  depends_on "fasttree" => "with-double"
   depends_on "megahit"
   depends_on "spades"
   depends_on "kraken"
