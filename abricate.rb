21713ab24e82381644ba7bb337757b879d25920e
Torsten Seemann <torsten.seemann@gmail.com>
2016-04-09 11:04:20 +1000

    ABRicate 0.3 (update)

--- abricate.rb
+++ abricate.rb
@@ -1,9 +1,13 @@
 class Abricate < Formula
+  desc "Find antiobiotic resistance genes in contigs"
   homepage "https://github.com/tseemann/abricate"
   # doi ""
   # tag "bioinformatics"
-  url "https://github.com/tseemann/abricate/archive/v0.2.tar.gz"
-  sha256 "65069fd0bffaae92b5144262ba9ca736a0ddb1f657ebe27bc3d1a5160ce16f66"
+
+  url "https://github.com/tseemann/abricate/archive/v0.3.tar.gz"
+  sha256 "b140212787d7f6a45806cd1c25c1b75214bc3291f2b568adfc80a61bb4e6d7d2"
+
+  head "https://github.com/tseemann/abricate.git"
 
   depends_on "Bio::Perl" => :perl
   depends_on "Time::Piece" => :perl
@@ -15,7 +19,7 @@ class Abricate < Formula
   end
 
   test do
-    assert_match "#{version}", shell_output("abricate --version")
+    assert_match version.to_s, shell_output("abricate --version")
     assert_match "rifampicin", shell_output("abricate --list 2>&1")
   end
 end
