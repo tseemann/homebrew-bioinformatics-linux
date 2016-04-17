8daf177acef4d20001964fed7a5ee622c9eac70d
Torsten Seemann <torsten.seemann@gmail.com>
2016-04-17 10:16:12 +1000

    VarSim 0.5.3 (new)

---
+++ varsim.rb
@@ -0,0 +1,22 @@
+class Varsim < Formula
+  desc "high-fidelity simulation validation framework for NGS"
+  homepage "https://github.com/bioinform/varsim"
+  # doi ""
+  # tag "bioinformatics"
+
+  url "https://github.com/bioinform/varsim/releases/download/v0.5.3/varsim-0.5.3.tar.gz"
+  sha256 "f5481cd890fd1ecf525c5a102170f12639af99f14de88dc112adebfbe2dd9f5a"
+
+  depends_on :java
+
+  def install
+    jar = "VarSim.jar"
+    libexec.install jar
+    bin.write_jar_script libexec/jar, "varsim"
+    bin.install Dir["var*.py"]
+  end
+
+  test do
+    assert_match "diploid", shell_output("varsim 2>&1", 1)
+  end
+end
