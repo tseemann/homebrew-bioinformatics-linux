6cd444288fafe4478c63fb0d9c47b472a480a9d7
Torsten Seemann <torsten.seemann@gmail.com>
2016-05-10 15:03:05 +1000

    Minia 2.0.7 (update)

--- minia2.rb
+++ minia2.rb
@@ -4,14 +4,17 @@ class Minia2 < Formula
   # doi "10.1186/1748-7188-8-22"
   # tag "bioinformatics"
 
-  url "http://gatb-tools.gforge.inria.fr/versions/bin/minia-2.0.3-Linux.tar.gz"
-  sha256 "4bb9e4c8e8084450db5d50b65b477f6b9d7d40ddf29f4c705ac14705a49a1ed1"
+  url "https://github.com/GATB/minia/releases/download/v2.0.7/minia-v2.0.7-bin-Linux.tar.gz"
+  sha256 "c51292aeaea4ff7c9c06a321776ba44ba4e613d5872555cc7985fb863ede4b72"
 
+  # for h5dump
+  depends_on "hdf5" => :optional
+
+  # minia 1.x series
   conflicts_with "minia"
 
   def install
     bin.install "bin/minia"
-    doc.install "doc/manual.pdf"
   end
 
   test do
