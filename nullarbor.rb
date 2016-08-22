5b651f3f010d28dcb7c58648bba533fd6e7f275c
Torsten Seemann <torsten.seemann@gmail.com>
2016-08-22 10:30:36 +1000

    Remove un-needed Perl module and pandoc

--- nullarbor.rb
+++ nullarbor.rb
@@ -13,12 +13,9 @@ class Nullarbor < Formula
   depends_on "Time::Piece" => :perl
   depends_on "Moo" => :perl
   depends_on "Data::Dumper" => :perl
-  depends_on "Spreadsheet::Read" => :perl
   depends_on "File::Copy" => :perl
   depends_on "YAML::Tiny" => :perl
 
-  depends_on "tseemann/bioinformatics-linux/snippy"
-  depends_on "tseemann/bioinformatics-linux/roary"
   depends_on "prokka"
   depends_on "fasttree" => "with-double"
   depends_on "megahit"
@@ -28,23 +25,14 @@ class Nullarbor < Formula
   depends_on "mlst"
   depends_on "abricate"
   depends_on "newick-utils"
+  depends_on "mash"
   depends_on "imagemagick" => "with-librsvg"
-  depends_on "pandoc" unless OS.linux? # GHC won't build
-  depends_on "mash" => :optional
+  depends_on "tseemann/bioinformatics-linux/snippy"
+  depends_on "tseemann/bioinformatics-linux/roary"
   depends_on "parsnp" => :optional
 
   def install
     prefix.install Dir["*"]
-  end
-
-  def caveats
-    if OS.linux?
-      <<-EOS.undent
-        You need to install the Linux system package for pandoc:
-        - Debian/Ubuntu: sudo apt-get install pandoc
-        - Fedora/RHEL: sudo yum install pandoc
-      EOS
-    end
   end
 
   test do
