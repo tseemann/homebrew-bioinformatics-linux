73d9f5a00ef7de470e910b20847880cac0046b30
Torsten Seemann <torsten.seemann@gmail.com>
2016-08-30 16:01:07 +1000

    Update NCBI blast urls

--- blast-legacy.rb
+++ blast-legacy.rb
@@ -1,13 +1,14 @@
 class BlastLegacy < Formula
+  desc "Legacy NCBI BLAST binaries"
   homepage "http://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Web&PAGE_TYPE=BlastDocs&DOC_TYPE=Download"
   # tag "bioinformatics"
   # doi "10.1093/nar/25.17.3389"
 
   if OS.mac?
-    url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/LATEST/blast-2.2.26-universal-macosx.tar.gz"
+    url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/legacy/2.2.26/blast-2.2.26-universal-macosx.tar.gz"
     sha256 "eabad6b37ded329a7edd14d650e0aedf7b88aa4bd1611a228d5191952d83f3b8"
   elsif OS.linux?
-    url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/LATEST/blast-2.2.26-x64-linux.tar.gz"
+    url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/legacy/2.2.26/blast-2.2.26-x64-linux.tar.gz"
     sha256 "8a2f986cf47f0f7cbdb3478c4fc7e25c7198941d2262264d0b6b86194b3d063d"
   else
     onoe "Unsupported operating system"
