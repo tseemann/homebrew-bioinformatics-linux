bdb948b9750a0b8b07b25bf70c1247f8dcdd51c3
Torsten Seemann <torsten.seemann@gmail.com>
2016-09-08 14:10:02 +1000

    LightAssembler (HEAD ONLY)

---
+++ light-assembler.rb
@@ -0,0 +1,20 @@
+class LightAssembler < Formula
+  desc "Lightweight resources NGS assembly algorithm"
+  homepage "https://github.com/SaraEl-Metwally/LightAssembler"
+  # doi ""
+  # tag "bioinformatics"
+
+  head "https://github.com/SaraEl-Metwally/LightAssembler.git"
+
+  depends_on "zlib" unless OS.mac?
+
+  def install
+    system "make", "k=127"
+    bin.install "LightAssembler"
+    doc.install "README.md"
+  end
+
+  test do
+    assert_match "LightAssembler", shell_output("LightAssembler 2>&1", 1)
+  end
+end
