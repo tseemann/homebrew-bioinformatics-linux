3b989f2362c9f01b68840cc2b036066846192a4c
Torsten Seemann <torsten.seemann@gmail.com>
2016-09-30 01:10:58 +1000

    cutadapt 1.11

--- cutadapt.rb
+++ cutadapt.rb
@@ -1,31 +1,21 @@
 class Cutadapt < Formula
+  include Language::Python::Virtualenv
+
   desc "Removes adapter sequences, primers, and poly-A tails"
   homepage "https://github.com/marcelm/cutadapt"
   # tag "bioinformatics"
   # doi "10.14806/ej.17.1.200"
 
-  url "https://github.com/marcelm/cutadapt/archive/v1.10.tar.gz"
-  sha256 "0019eebf971c32c356fc4cffd22dca4040010f097950e9ce578c29e1c92e5b59"
+  url "https://files.pythonhosted.org/packages/47/bf/9045e90dac084a90aa2bb72c7d5aadefaea96a5776f445f5b5d9a7a2c78b/cutadapt-1.11.tar.gz"
+  sha256 "11d0af9b39d6d9de6d186f113c7b361c24e7432425b9fa68fb2857a4656890cf"
 
   head "https://github.com/marcelm/cutadapt.git"
 
-  depends_on :python if MacOS.version <= :snow_leopard
-
-  resource "Cython" do
-    url "https://pypi.python.org/packages/c6/fe/97319581905de40f1be7015a0ea1bd336a756f6249914b148a17eefa75dc/Cython-0.24.1.tar.gz"
-    sha256 "84808fda00508757928e1feadcf41c9f78e9a9b7167b6649ab0933b76f75e7b9"
-  end
+  depends_on :python3
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    resource("Cython").stage do
-      system "python", *Language::Python.setup_install_args(libexec/"vendor")
-    end
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
-    libexec.install Dir[libexec/"bin/*"]
-    libexec.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-    (bin/"cutadapt").write_env_script(libexec/"bin/cutadapt", :PYTHONPATH => ENV["PYTHONPATH"])
+    virtualenv_create(libexec, "python3")
+    virtualenv_install_with_resources
   end
 
   test do
