22f70bbb4e09899a25ddfbaa34429b92ad776e0a
Torsten Seemann <torsten.seemann@gmail.com>
2017-03-13 13:42:55 +1100

    gemma - not zlib dep on macos

--- gemma.rb
+++ gemma.rb
@@ -7,7 +7,7 @@ class Gemma < Formula
   url "http://www.xzlab.org/software/gemma-0.94.tar.gz"
   sha256 "08c7569bb075ce62c89c4ec4e003b7d256cd435e547b89d68fc23aec184d5b35"
 
-  depends_on "zlib"
+  depends_on "zlib" unless OS.mac?
   depends_on "openblas"
   depends_on "gsl"
 
