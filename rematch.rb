aaf0f8be20ed6064a7166c97747027dfa86a5832
Torsten Seemann <torsten.seemann@gmail.com>
2017-08-20 09:35:45 +1000

    rematch 3.2

--- rematch.rb
+++ rematch.rb
@@ -1,17 +1,18 @@
 class Rematch < Formula
   desc "Amplicon read mapping and consensus"
   homepage "https://github.com/B-UMMI/ReMatCh"
+  # doi ""
   # tag "bioinformatics"
 
-  url "https://github.com/B-UMMI/ReMatCh/archive/v2.0.tar.gz"
-  sha256 "64e5c8f359c971e419888bf79c4e99116989d6b48d76a87f67c5a240519eaea8"
+  url "https://github.com/B-UMMI/ReMatCh/archive/v3.2.tar.gz"
+  sha256 "60cd3dbdeed519aab397fa19583f84d9e614240dc7525231fcb9ceaf3509ce42"
   head "https://github.com/B-UMMI/ReMatCh.git"
 
   depends_on "ascp"
   depends_on "bowtie2"
   depends_on "samtools"
   depends_on "bcftools"
-  depends_on "wget"
+#  depends_on "wget"
   depends_on "blast"
 
   def install
