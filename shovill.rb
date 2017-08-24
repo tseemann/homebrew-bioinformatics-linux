ea9ef6e3e5fef985bb88c832c55582b2977abbad
Torsten Seemann <torsten.seemann@gmail.com>
2017-08-24 18:16:16 +1000

    shovill 0.6.0

--- shovill.rb
+++ shovill.rb
@@ -1,15 +1,16 @@
 class Shovill < Formula
-  desc "Faster smarter Spades-based aseembly"
+  desc "Faster smarter Spades-based de novo genome assembly"
   homepage "https://github.com/tseemann/shovill"
   # doi ""
   # tag "bioinformatics"
 
-  url "https://github.com/tseemann/shovill/archive/v0.5.tar.gz"
-  sha256 "aa01128e98d014725f465a9a2a7c692bb042d0c609dc58a2e05b40f98c1165da"
+  url "https://github.com/tseemann/shovill/archive/v0.6.0.tar.gz"
+  sha256 "daaf22d96660c60d685b935f49c9e57d0893eed96beb1ba0ba883b7889b00c0c"
 
   head "https://github.com/tseemann/shovill.git"
 
   depends_on "Time::Piece" => :perl
+  depends_on "File::Slurp" => :perl
 
   depends_on "bwa"
   depends_on "kmc"
@@ -26,5 +27,6 @@ class Shovill < Formula
 
   test do
     assert_match "Faster", shell_output("#{bin}/shovill -h 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/shovill --version 2>&1", 0)
   end
 end
