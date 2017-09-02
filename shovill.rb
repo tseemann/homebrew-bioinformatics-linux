8d3cc37b4327d8c9c732cd3fa49c36541e291cf7
Torsten Seemann <torsten.seemann@gmail.com>
2017-09-02 16:20:12 +1000

    shovill 0.7

--- shovill.rb
+++ shovill.rb
@@ -1,11 +1,11 @@
 class Shovill < Formula
-  desc "Faster smarter Spades-based de novo genome assembly"
+  desc "Faster Spades-based de novo genome assembly"
   homepage "https://github.com/tseemann/shovill"
   # doi ""
   # tag "bioinformatics"
 
-  url "https://github.com/tseemann/shovill/archive/v0.6.0.tar.gz"
-  sha256 "daaf22d96660c60d685b935f49c9e57d0893eed96beb1ba0ba883b7889b00c0c"
+  url "https://github.com/tseemann/shovill/archive/v0.7.0.tar.gz"
+  sha256 "38e43e088f5ec898678d9056f658b97612cd888cf80eae47fcdddf308aaaac89"
 
   head "https://github.com/tseemann/shovill.git"
 
@@ -13,11 +13,12 @@ class Shovill < Formula
   depends_on "File::Slurp" => :perl
 
   depends_on "bwa"
-  depends_on "kmc"
-  depends_on "samtools"
   depends_on "flash"
+  depends_on "kmc"
   depends_on "lighter"
+  depends_on "pigz"
   depends_on "pilon"
+  depends_on "samtools"
   depends_on "seqtk"
   depends_on "trimmomatic"
 
@@ -26,7 +27,7 @@ class Shovill < Formula
   end
 
   test do
-    assert_match "Faster", shell_output("#{bin}/shovill -h 2>&1", 1)
-    assert_match version.to_s, shell_output("#{bin}/shovill --version 2>&1", 0)
+    assert_match "Faster", shell_output("#{bin}/shovill --help 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/shovill --version 2>&1")
   end
 end
